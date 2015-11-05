e668468b1acefca8b52052a57b0019c99c97067b
Ken Lee <kdblitz@gmail.com>
2015-11-06 01:57:44 +0800

    Added info handling.

--- js/models/InfoWindow.js
+++
@@ -1,9 +0,0 @@
-import ColorGenerator from './generators/ColorGenerator';
-
-export default class InfoWindow {
-  constructor(content) {
-    this.info = new google.maps.InfoWindow({
-      content:`<div><h3 style="color:${ColorGenerator.getRandomColor()}">${content}</h3></div>`
-    });
-  }
-}
--- js/models/Marker.js
+++ js/models/Marker.js
@@ -1,5 +1,42 @@
+import ColorGenerator from './generators/ColorGenerator';
 import LabelGenerator from './generators/LabelGenerator';
-import InfoWindow from './InfoWindow';
+
+class InfoWindow {
+  constructor(content) {
+    this.id = _.uniqueId('InfoWindow')
+    this.content = content;
+    this.info = new google.maps.InfoWindow({
+      content:`<div id="${this.id}"></div>`
+    });
+    // this.viewMode();
+  }
+
+  getDom() {
+    return document.querySelector(`#${this.id}`);
+  }
+
+  viewMode() {
+    let $info = this.getDom()
+    $info.innerHTML = '';
+
+    let $label = document.createElement('h3');
+    $label.innerHTML = this.content;
+    $label.style.color = ColorGenerator.getRandomColor();
+
+    let $editButton = document.createElement('button');
+    $editButton.innerHTML = 'Edit me';
+    $editButton.addEventListener('click', (() => {
+      this.editMode();
+    }).bind(this) )
+
+    $info.appendChild($label);
+    $info.appendChild($editButton);
+  }
+
+  editMode() {
+    this.getDom().innerHTML = `<input type="text" value="${this.content}">`;
+  }
+}
 
 export default class Marker {
   constructor (map, latLng, description = LabelGenerator.next().value) {
@@ -8,7 +45,7 @@ export default class Marker {
       map,
       title:description
     });
-    this.infoWindow = new InfoWindow(description).info;
+    this.infoWindow = new InfoWindow(description);
     this.addListener();
     this.map = map;
     this.description = description;
@@ -16,7 +53,8 @@ export default class Marker {
 
   addListener() {
     this.marker.addListener('click', () => {
-      this.infoWindow.open(this.map, this.marker);
+      this.infoWindow.info.open(this.map, this.marker);
+      this.infoWindow.viewMode();
     });
   }
 }

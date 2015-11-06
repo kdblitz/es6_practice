32d0bbe79882137a4453071a0b103fbda4a052f2
Ken Lee <ken.lee@nokia.com>
2015-11-06 21:43:31 +0800

    Localstorage implementation.

--- js/models/maps/MarkerList.js
+++ js/models/maps/MarkerList.js
@@ -4,6 +4,18 @@ export default class MarkerList {
     this.changeListeners = new Set();
   }
 
+  saveData() {
+    console.log(
+      JSON.stringify([...this.markers].map( (marker)=>marker.marker.position )
+        .map(position => {return{lng:position.lng(), lat:position.lat()}}))
+    );
+    // console.log(this.markers.map( (marker)=> {
+    //   return marker;
+    //   // return {lat: marker.marker.position.lat(),
+    //   // lng: marker.marker.position.lng()};
+    // });
+  }
+
   addChangeListener(listener) {
     this.changeListeners.add(listener);
   }
@@ -13,6 +25,7 @@ export default class MarkerList {
     marker.setRemoveEvent(this.removeMarker.bind(this));
 
     this.markers.add(marker);
+    this.saveData();
     this.notify();
   }
 
